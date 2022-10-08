5ab776932f8e30b6f133a680a419f8cc391f0153
Allan Scala <allanscala@gmail.com>
2022-10-08 20:49:08 -0300

    feat: create provider repository

---
+++ src/main/java/com/example/inventorycontrol/api/repository/ProviderRepository.java
@@ -0,0 +1,13 @@
+package com.example.inventorycontrol.api.repository;
+
+import com.example.inventorycontrol.api.model.ProviderModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ProviderRepository extends JpaRepository<ProviderModel, UUID> {
+    Optional<ProviderModel> findByFantasyName(String fantasyName);
+}
